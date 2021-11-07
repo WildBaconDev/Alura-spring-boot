fa22a37cc02f53f9125bef328b42e7e6f9e3f94a
Davi <davi.mirandagonc@gmail.com>
2021-11-06 22:13:36 -0300

    Aula 7 e fim

--- src/main/java/br/com/alura/forum/config/security/SecurityConfigurations.java
+++ src/main/java/br/com/alura/forum/config/security/SecurityConfigurations.java
@@ -49,6 +49,7 @@ public class SecurityConfigurations extends WebSecurityConfigurerAdapter {
 			.antMatchers(HttpMethod.GET, "/topicos").permitAll()
 			.antMatchers(HttpMethod.GET, "/topicos/*").permitAll()
 			.antMatchers(HttpMethod.POST, "/auth").permitAll()
+			.antMatchers(HttpMethod.GET, "/actuator/**").permitAll()
 			.anyRequest().authenticated()
 			.and().csrf().disable()
 			.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
@@ -58,6 +59,9 @@ public class SecurityConfigurations extends WebSecurityConfigurerAdapter {
 	
 //	Configurações de recursos estáticos
 	@Override
-	public void configure(WebSecurity web) throws Exception { }
+	public void configure(WebSecurity web) throws Exception {
+	    web.ignoring()
+	        .antMatchers("/**.html", "/v2/api-docs", "/webjars/**", "/configuration/**", "/swagger-resources/**");
+	}
 	
 }
